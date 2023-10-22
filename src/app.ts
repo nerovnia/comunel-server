ee3cb22a43f1656349ee1812032ac5a034945251
Volodymyr Nerovnia <nerv@i.ua>
2023-10-22 03:53:09 +0300

    Added server graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,16 @@ app.get('/healthcheck', (req, res) => {
   res.send({ message: 'Success' })
 })
 
+// graceful shutdown
+const listeners = ['SIGINT', 'SIGTERM']
+listeners.forEach((signal) => {
+  process.on(signal, async () => {
+    console.log('\nServer closing...');
+    await app.close()
+    process.exit(0)
+  })
+})
+
 async function main() {
   await app.listen({
     port: 8000,
